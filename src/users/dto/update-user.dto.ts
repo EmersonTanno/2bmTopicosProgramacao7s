82ad7f74685f65c85e42d0df62f8d058d48f811f
Emerson Tanno <tannobrizotto@gmail.com>
2025-05-28 10:52:10 -0300

    update: update-user.dto - adding fields

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,20 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { IsOptional, IsString, Matches } from 'class-validator';
+import { Role } from 'src/enum/roles.enum';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+    @IsString()
+    @IsOptional()
+    name: string;
+
+    @IsString()
+    @IsOptional()
+    @Matches(/\S/, { message: 'A nova senha não pode conter apenas espaços' })
+    password: string;
+
+    @IsString()
+    @IsOptional()
+    roles: Role[];
+
+}
